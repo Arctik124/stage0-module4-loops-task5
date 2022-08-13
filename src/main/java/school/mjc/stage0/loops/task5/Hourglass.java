316fb8a8ffa8d585b0e04c04b9b427a8251bf6f4
Artyom <artvit12@gmail.com>
2022-08-13 11:00:49 +0600

    Solution

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -1,7 +1,29 @@
 package school.mjc.stage0.loops.task5;
 
-public class Hourglass {
-    public void printHourglassOfGivenSize(int height) {
+/**
+ * Write a program write to console filled Hourglass of a given length(it should consist of '8'-s).
+ */
 
+public class Hourglass {
+    public static void main(String[] args) {
+        Hourglass hourglass = new Hourglass();
+        hourglass.printHourglassOfGivenSize(5);
+        System.out.println("**************");
+        hourglass.printHourglassOfGivenSize(6);
+    }
+
+    public void printHourglassOfGivenSize(int height) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < height; j++) {
+                if ((j >= i && j < height - i) || (i >= height / 2 && (j < i + 1 && i + 1 >= height - j))) {
+                    System.out.print(8);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("\n");
+
+        }
     }
 }
+
--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -1,7 +1,20 @@
 package school.mjc.stage0.loops.task5;
 
+/**
+ * Write a program will write to console a rectangular triangle(cathetus should be of a given size)
+ * of a given length(it should consist of '8'-s).
+ */
 public class Triangle {
     public void printTriangle(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 1; j <= cathetusLength; j++) {
+                if (i >= j  ) {
+                    System.out.print(8);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("\n");
+        }
     }
 }
